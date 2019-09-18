b4498507794106ec1178e820648cc818e1eba989
Matt McCarthy <mattmc318@gmail.com>
2019-09-18 10:59:01 -0500

    Completed basic functionality for image slider

--- home/static/home/js/image_slider.js
+++ home/static/home/js/image_slider.js
@@ -1,64 +1,152 @@
 $(document).ready(() => {
   var width = $(window).width();
+  var slideLeft = true;
 
-  function getImages(context={
-    'year': $('#imageSlider .year').text(),
-    'page': $('#imageSlider input[name="page"]').val(),
+  var $year = $('#imageSlider .year');
+  var $page = $('#imageSlider input[name="page"]');
+
+  function getImages(context = {
+    'year': $year.text(),
+    'page': $page.val(),
     'per_page': width < 768 ? 1 : width >= 768 && width < 992 ? 2 : 3,
   }) {
+    var isWidth = $('#imageSlider').width();
+
     $.ajax('/image_slider/', {
       data: {
-        'year': $('#imageSlider .year').text(),
-        'page': $('#imageSlider input[name="page"]').val(),
+        'year': $year.text(),
+        'page': $page.val(),
         'per_page': width < 768 ? 1 : width >= 768 && width < 992 ? 2 : 3,
       },
       statusCode: {
         500: () => {
-          var page = $('#imageSlider input[name="page"]').val();
-          $('#imageSlider input[name="page"]').val(page - 1 > 0 ? --page : 1);
+          $('#imageSlider .images').effect('shake');
+          var page = $page.val();
+          $page.val(page - 1 > 0 ? --page : 1);
         }
       },
       success: (data) => {
-        $('#imageSlider .images').html(data);
+        let $images = $('#imageSlider .images');
+
+        if (slideLeft) {
+          $images.append(data);
+
+          let $first = $images.children('.is-row:first-child');
+          let $last = $images.children('.is-row:last-child');
+
+          $first.animate({
+            'left': `-${isWidth}px`,
+          }, 1000);
+          $last.css('left', `${isWidth}px`).animate({
+            'left': `0px`,
+          }, 1000);
+
+          setTimeout(() => {
+            $first.remove();
+          }, 1000);
+        } else {
+          $images.prepend(data);
+
+          let $first = $images.children('.is-row:first-child');
+          let $last = $images.children('.is-row:last-child');
+
+          $first.css('left', `-${isWidth}px`).animate({
+            'left': `0px`,
+          }, 1000);
+          $last.animate({
+            'left': `${isWidth}px`,
+          }, 1000);
+
+          setTimeout(() => {
+            $last.remove();
+          }, 1000);
+        }
       }
     });
   }
 
   getImages();
 
-  $(window).on('resize orientationchange', () => {
+  function getViewportSize() {
+    if (width >= 992) {
+      return 'lg-xl';
+    } else if (width >= 768) {
+      return 'md';
+    } else {
+      return 'xs-sm';
+    }
+  }
+
+  var oldViewportSize = getViewportSize();
+
+  function refreshSlider() {
     width = $(window).width();
-    getImages();
-  });
+    var viewportSize = getViewportSize();
 
+    if (oldViewportSize !== viewportSize) {
+      $page.val(1);
+      getImages();
+    }
+
+    oldViewportSize = viewportSize;
+  }
+
+  $(window).on('resize orientationchange', refreshSlider);
+
+  var page = 1;
   $('#imageSlider .nav-left').click(() => {
-    var page = $('#imageSlider input[name="page"]').val();
-    $('#imageSlider input[name="page"]').val(page - 1 > 0 ? --page : 1);
+    if (debounce) {
+      debounce = false;
 
-    getImages({
-      'year': $('#imageSlider .year').text(),
-      'page': page,
-      'per_page': width < 768 ? 1 : width >= 768 && width < 992 ? 2 : 3,
-    });
+      setTimeout(() => {
+        debounce = true;
+      }, 1000);
+
+      slideLeft = false;
+      page = Number($page.val());
+
+      if (page === 1) {
+        $('#imageSlider .images').effect('shake');
+        return;
+      }
+
+      $page.val(page - 1 > 0 ? --page : 1);
+
+      getImages({
+        'year': $year.text(),
+        'page': page,
+        'per_page': width < 768 ? 1 : width >= 768 && width < 992 ? 2 : 3,
+      });
+    }
   });
 
+  var debounce = true;
   $('#imageSlider .nav-right').click(() => {
-    var page = $('#imageSlider input[name="page"]').val();
-    $('#imageSlider input[name="page"]').val(++page);
+    if (debounce) {
+      debounce = false;
 
-    getImages({
-      'year': $('#imageSlider .year').text(),
-      'page': page,
-      'per_page': width < 768 ? 1 : width >= 768 && width < 992 ? 2 : 3,
-    });
+      setTimeout(() => {
+        debounce = true;
+      }, 1000);
+
+      slideLeft = true;
+      page = $page.val();
+      $page.val(++page);
+
+      getImages({
+        'year': $year.text(),
+        'page': page,
+        'per_page': width < 768 ? 1 : width >= 768 && width < 992 ? 2 : 3,
+      });
+    }
   });
 
-  var dropdown = false;
+  var dropdownExpanded = false;
   var $dropdown = $('#imageSlider .dropdown');
   var $scroll = $dropdown.children('.scroll');
 
-  $dropdown.children('.header').click(() => {
-    if (dropdown) {
+  function toggleDropdown() {
+    if (dropdownExpanded) {
       $dropdown.animate({
         'height': '2rem',
       }, 500);
@@ -66,20 +154,34 @@ $(document).ready(() => {
       $scroll.show();
       $dropdown.animate({
         'height': '330px',
-      }, 500).css('background-color', '#666666');
+      }, 500);
     }
 
     var $chevron = $('#imageSlider .dropdown .chevron');
     setTimeout(() => {
-      if (dropdown) {
+      if (dropdownExpanded) {
         $chevron.html('<i class="fa fa-chevron-down" aria-hidden="true"></i>');
-        $dropdown.css('background-color', '#6666667f');
         $scroll.hide();
       } else {
         $chevron.html('<i class="fa fa-chevron-up" aria-hidden="true"></i>');
       }
 
-      dropdown = !dropdown;
+      dropdownExpanded = !dropdownExpanded;
     }, 500);
+  }
+
+  $dropdown.children('.header').click(toggleDropdown);
+
+  $scroll.children('li').click(function (event) {
+    event.stopPropagation();
+
+    let year = $(this).text();
+    $('#imageSlider .dropdown .year').html(year);
+
+    slideLeft = true;
+    $page.val(1);
+
+    toggleDropdown();
+    getImages();
   });
 });
--- home/static/home/js/script.js
+++ home/static/home/js/script.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
   $('a.external-link')
-    .after(' <i class="fas fa-external-link-alt" title="External Link"></i>');
+    .append(' <i class="fas fa-external-link-alt" title="External Link"></i>');
 });
