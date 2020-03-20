46f63ca8db218d6c2261f8b3a2e115600ff1e664
Matt McCarthy <mattmc318@gmail.com>
2020-03-19 20:32:06 -0500

    Fixed navbar dropdown bug; removed trailing spaces from static files

--- home/static/home/js/gallery.js
+++ home/static/home/js/gallery.js
@@ -18,7 +18,7 @@ $(document).ready(() => {
     var inner = document.createElement('p');
     inner.style.width = "100%";
     inner.style.height = "200px";
-  
+
     var outer = document.createElement('div');
     outer.style.position = "absolute";
     outer.style.top = "0px";
@@ -28,15 +28,15 @@ $(document).ready(() => {
     outer.style.height = "150px";
     outer.style.overflow = "hidden";
     outer.appendChild(inner);
-  
+
     document.body.appendChild(outer);
     var w1 = inner.offsetWidth;
     outer.style.overflow = 'scroll';
     var w2 = inner.offsetWidth;
     if (w1 == w2) w2 = outer.clientWidth;
-  
+
     document.body.removeChild(outer);
-  
+
     return (w1 - w2);
   };
 
--- home/static/home/js/script.js
+++ home/static/home/js/script.js
@@ -1,4 +1,20 @@
 $(document).ready(() => {
+  console.log('This site brought to you by McCarthy Web Design.');
+  console.log('https://mccarthy-web-design.000webhostapp.com/');
+
+  // external link icon
   $('a.external-link')
     .append(' <i class="fas fa-external-link-alt" title="External Link"></i>');
+
+  // navbar menu
+  $('#navbarCollapseButton').click(function () {
+    $('#navbarCollapse').slideToggle();
+  });
+  $(window).on('resize orientationchange', function () {
+    if ($(this).width() >= breakpointMd) {
+      $('#navbarCollapse').css('display', 'inline');
+    } else {
+      $('#navbarCollapse').hide();
+    }
+  });
 });
