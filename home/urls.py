3828b09233ae2079ede9d0a83f792ef61f5099c2
Matt McCarthy <matt@mccarthycode.com>
2022-07-23 17:49:09 -0500

    Remove Contests page

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('contests/', views.contests, name='contests'),
+    # path('contests/', views.contests, name='contests'),
     path('contribute/', views.contribute, name='contribute'),
     path('tickets/', views.tickets, name='tickets'),
     path('image_slider/', views.image_slider, name='image_slider'),
